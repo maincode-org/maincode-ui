52a9dcf50cab2f4039cf4a81351fbe3a3bbbdf4c
MarkKragerup <markido@live.dk>
2021-09-14 22:59:26 +0200

    Removed some user selection.

--- src/components/live-code-editor/LiveCodeEditor.tsx
+++ src/components/live-code-editor/LiveCodeEditor.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import { IonButton } from '@ionic/react';
+
+export const ExampleButton: React.FC = () => {
+  return <IonButton className='ion-float-end'>Example button!</IonButton>;
+};
 
 type IProps = {
   className?: string;
 };
 
 const LiveCodeEditor: React.FC<IProps> = ({ className }) => {
+  const code = `<ExampleButton/>`;
+  const scope = { ExampleButton };
+
   return (
-    <LiveProvider className={className} code='<strong>Hello World!</strong>'>
+    <LiveProvider className={className} code={code} scope={scope}>
       <LiveEditor />
       <LiveError />
       <LivePreview />
