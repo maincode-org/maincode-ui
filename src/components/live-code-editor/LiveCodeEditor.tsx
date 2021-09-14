f833995cd08ab8cd361047680bd63c70fca27596
MarkKragerup <markido@live.dk>
2021-09-14 22:23:57 +0200

    Removed some user selection.

--- src/components/live-code-editor/LiveCodeEditor.tsx
+++ src/components/live-code-editor/LiveCodeEditor.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 type IProps = {
   className?: string;
 };
 
 const LiveCodeEditor: React.FC<IProps> = ({ className }) => {
-  return <div className={className}>Ship and run</div>;
+  return (
+    <LiveProvider className={className} code='<strong>Hello World!</strong>'>
+      <LiveEditor />
+      <LiveError />
+      <LivePreview />
+    </LiveProvider>
+  );
 };
 export default LiveCodeEditor;
