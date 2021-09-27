e456bfc9fe2fd82e3951e2b26c38d90b471c9dd8
Markido <markido@live.dk>
2021-09-27 13:59:12 +0200

    Refactored some documentation parsing

--- src/components/code-components/live-code-editor/LiveCodeEditor.tsx
+++ src/components/code-components/live-code-editor/LiveCodeEditor.tsx
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from 'react';
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
-import styles from './live-code-editor.module.css';
+import { Language } from 'prism-react-renderer';
 import darkTheme from 'prism-react-renderer/themes/nightOwl';
 import lightTheme from 'prism-react-renderer/themes/nightOwlLight';
 import CopyButton from '../copy-button/CopyButton';
-import { Language } from 'prism-react-renderer';
+import styles from './live-code-editor.module.css';
 import { EThemeModes, ThemeContext } from 'contexts/theme';
 
 type IProps = {
