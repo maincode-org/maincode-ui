4f6babe507f8809bae119532689a0c2b5649557d
mathias <mathias.hoeyrup@gmail.com>
2021-10-28 14:11:03 +0200

    Improved PrettyUL to PrettyList - now takes ordering as prop

--- documentation-app/src/structure/basic-components.tsx
+++ documentation-app/src/structure/basic-components.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CopyArea, IEntityCategory } from 'maincode-ui';
+import { CopyArea, PrettyList, IEntityCategory } from 'maincode-ui';
 import { prepareURLPrefixComponents } from './url-prefix';
 import { liveEditorDocumentation, liveEditorPreview } from '../pages/component-pages/code/live-editor-page/documentation-page';
 
@@ -12,6 +12,28 @@ export const basicComponentPages: IEntityCategory = prepareURLPrefixComponents({
       preview: { element: <CopyArea command={'npm install maincode-ui'} /> },
       description: <p>Displays single commands with controls for seamless clipboard copying</p>,
       props: [{ title: 'Animated', description: `If <code>true</code>, the alert will animate`, required: true, type: 'boolean', defaultValue: 'true' }],
+      styles: [
+        { propertyName: `<code>glass-bg</code>`, description: 'This is a glass effect on the background' },
+        { propertyName: `<code>theme-border</code>`, description: 'Border matching the theme' },
+      ],
+    },
+    {
+      url: '/pretty-list',
+      title: 'Pretty List',
+      preview: { element: <PrettyList ordering='alphabetic' items={['Mark er nice', 'Mark er noob', 'Mark er optical illusion']} /> },
+      description: <p>Displays a list of elements akin to either the ol or the ul HTML element.</p>,
+      codeExamples: [
+        {
+          title: 'Title',
+          description: <p>Description</p>,
+          code: "<PrettyList ordering='alphabetic' items={['Mark er nice', 'Mark er noob', 'Mark er optical illusion']} />",
+          scope: { PrettyList },
+        },
+      ],
+      props: [
+        { title: 'items', description: 'The list items', required: true, type: '(React.ReactNode | string)[]', defaultValue: '[]' },
+        { title: 'isOrdered', description: 'Whether or not the list items are ordered', required: false, type: 'boolean', defaultValue: 'false' },
+      ],
       styles: [
         { propertyName: `<code>glass-bg</code>`, description: 'This is a glass effect on the background' },
         { propertyName: `<code>theme-border</code>`, description: 'Border matching the theme' },
