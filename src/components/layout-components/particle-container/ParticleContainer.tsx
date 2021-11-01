f874673881f172db49c12ec277a98fa4c8f9d4bb
Markido <markido@live.dk>
2021-11-01 17:38:51 +0100

    Particles work in doc

--- src/components/layout-components/particle-container/ParticleContainer.tsx
+++ src/components/layout-components/particle-container/ParticleContainer.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Particles from 'react-particles-js';
+import config from './lean-particlesjs-config';
+import styles from './maincode-website.module.css';
 
 type IProps = {
   className?: string;
 };
 
 const ParticleContainer: React.FC<IProps> = ({ className = '' }) => {
-  return <p>hi {className}</p>;
+  const [particleConfig, setParticleConfig] = useState(config);
+
+  // HACK: update the object after first rendering, to spread out the particles and prevent them raining down..
+  useEffect(() => {
+    setTimeout(() => setParticleConfig({ ...particleConfig, move: { ...particleConfig.move, speed: 0.3 } }), 200);
+  }, [particleConfig]);
+
+  return <Particles className={className} canvasClassName={styles.backgroundParticles} params={particleConfig} />;
 };
 export default ParticleContainer;
