79b775ef1e85e9941bef5e4b98a34ab2b1e28fe3
Markido <markido@live.dk>
2021-11-01 20:15:12 +0100

    Particle container can have background and children

--- documentation-app/src/pages/component-pages/layout/particle-container/documentation-page.tsx
+++ documentation-app/src/pages/component-pages/layout/particle-container/documentation-page.tsx
@@ -1,9 +1,12 @@
 import { IDocumentationPageContent, ParticleContainer } from 'maincode-ui';
+import styles from './particle-container-doc.module.css';
 
 export const particleContainerDocumentation: IDocumentationPageContent = {
   customContent: (
     <>
-      <ParticleContainer />
+      <ParticleContainer className={`${styles.position} ${styles.bgImgRedBlue}`}>
+        <h3>Test</h3>
+      </ParticleContainer>
     </>
   ),
 };
--- src/components/layout-components/particle-container/ParticleContainer.tsx
+++ src/components/layout-components/particle-container/ParticleContainer.tsx
@@ -7,17 +7,18 @@ type IProps = {
   className?: string;
 };
 
-const ParticleContainer: React.FC<IProps> = ({ className = '' }) => {
+const ParticleContainer: React.FC<IProps> = ({ className = '', children }) => {
   const [particleConfig, setParticleConfig] = useState<typeof config | undefined>(undefined);
 
   // HACK: update the object after first rendering, to spread out the particles and prevent them raining down. Cause of bug unknown.
   useEffect(() => {
-    setTimeout(() => setParticleConfig(config), 100);
+    setTimeout(() => setParticleConfig(config), 300);
   }, []);
 
   return (
     <div className={`${className} ${styles.backgroundParticles}`}>
       {particleConfig && <Particles className={styles.animation} canvasClassName={styles.backgroundParticles} params={particleConfig} />}
+      {children}
     </div>
   );
 };
