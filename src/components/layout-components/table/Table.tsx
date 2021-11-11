843a092a23b052db2f3bd48fcca3449dde1cb131
Markido <markido@live.dk>
2021-11-11 11:53:54 +0100

    Applied changes to table and toolkit doc

--- src/components/layout-components/table/Table.tsx
+++ src/components/layout-components/table/Table.tsx
@@ -4,7 +4,7 @@ import parse from 'html-react-parser';
 
 type IProps = {
   title: string | JSX.Element;
-  properties: ({ label: string; value: string } | undefined)[];
+  properties: ({ label: string | JSX.Element; value: string | JSX.Element } | undefined)[];
   className?: string;
 };
 
@@ -13,10 +13,9 @@ const Table: React.FC<IProps> = ({ title, properties, className = '' }) => {
     <table className={`w-full glass-bg shadow-md ${styles.table} ${className}`}>
       <thead className='theme-border w-full rounded-md'>
         <tr>
-          <td className='theme-border'>
-            <h4 className='p-1'>{title}</h4>
-          </td>
-          <td className='theme-border' />
+          <th colSpan={2} className='theme-border'>
+            <h4 className='p-2 pl-1'>{title}</h4>
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -24,8 +23,8 @@ const Table: React.FC<IProps> = ({ title, properties, className = '' }) => {
           properties.map((p, i) =>
             p ? (
               <tr key={i} className={styles.row}>
-                <td className={`${styles.left} p-1 theme-border`}>{parse(p.label)}</td>
-                <td className={`${styles.right} p-1 theme-border`}>{parse(p.value)}</td>
+                <td className={`${styles.left} p-1 theme-border`}>{typeof p.label === 'string' ? parse(p.label) : p.label}</td>
+                <td className={`${styles.right} p-1 theme-border`}>{typeof p.value === 'string' ? parse(p.value) : p.value}</td>
               </tr>
             ) : undefined
           )}
