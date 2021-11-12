989dca4ee95788da4dea14e53899f498e4fef29e
Markido <markido@live.dk>
2021-11-12 15:18:12 +0100

    Review of Live Code Editor doc complete

--- documentation-app/src/pages/component-pages/code/live-editor-page/documentation-page.tsx
+++ documentation-app/src/pages/component-pages/code/live-editor-page/documentation-page.tsx
@@ -24,16 +24,16 @@ export const liveEditorDocumentation: IDocumentationPageContent = {
   codeExamples: [
     {
       title: 'Basic example',
-      description: <>We can use the component to quickly render simple JSX. For example, we can showcase the use of an Ionic button like below.</>,
+      description: <>We can use the component to quickly render simple JSX. For example, we can showcase the use of an Ionic button like below</>,
       code: '<IonButton>An ion button!</IonButton>',
       scope: { IonButton },
     },
     {
-      description: <>In order to produce the above example, we can initiate the live code editor with the code below.</>,
+      description: <>In order to produce the above example, we can initiate the live code editor with the code below</>,
       outro: (
         <InfoArea>
           The editor must have <code>IonButton</code> in scope in order to preview it. However, any CSS and classes in scope for the parent component or page will <b>automatically</b> be in scope for
-          the code. This extends to theming, and is why the button has styling according to the theme of this page.
+          the code. This extends to theming, and is why the button has styling according to the theme of this page
         </InfoArea>
       ),
       enablePreview: false,
@@ -43,14 +43,14 @@ export const liveEditorDocumentation: IDocumentationPageContent = {
       title: 'Advanced example',
       description: (
         <>
-          Spawning a live code editor with no inline requires a &quot;<code>render()</code>&quot; call in the code string.
+          Spawning a live code editor with no inline requires a &quot;<code>render()</code>&quot; call in the code string
         </>
       ),
       code: advancedCodeExample,
       enablePreview: false,
     },
     {
-      description: <>The above example code will produce a code example just like the one shown below here.</>,
+      description: <>The above example code will produce a code example just like the one shown below here</>,
       code: jsxExample,
       noInline: true,
     },
--- documentation-app/src/pages/component-pages/code/live-editor-page/styles.ts
+++ documentation-app/src/pages/component-pages/code/live-editor-page/styles.ts
@@ -3,18 +3,18 @@ import { IStyleDetail } from 'maincode-ui';
 export const liveEditorStyles: IStyleDetail[] = [
   {
     propertyName: '--wrapper-min-height',
-    description: 'Controls the minimum height of the wrapper.',
+    description: 'Controls the minimum height of the wrapper',
   },
   {
     propertyName: '--wrapper-max-height',
-    description: 'Controls the maximum height of the wrapper.',
+    description: 'Controls the maximum height of the wrapper',
   },
   {
     propertyName: '--border-radius',
-    description: 'Controls the border radius for the outer box of the editor.',
+    description: 'Controls the border radius for the outer box of the editor',
   },
   {
     propertyName: '--border-color',
-    description: 'Controls the color of the split- line between preview and code.',
+    description: 'Controls the color of the split- line between preview and code',
   },
 ];
