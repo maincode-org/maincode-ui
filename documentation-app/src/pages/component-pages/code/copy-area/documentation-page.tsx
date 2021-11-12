ed42bcfe3c1fa39a4d06e678bb6642c3ce113c24
Markido <markido@live.dk>
2021-11-12 14:08:34 +0100

    Copy area review finished

--- documentation-app/src/pages/component-pages/code/copy-area/documentation-page.tsx
+++ documentation-app/src/pages/component-pages/code/copy-area/documentation-page.tsx
@@ -1,18 +1,20 @@
 import { IDocumentationPageContent, CopyArea } from 'maincode-ui';
 
 export const copyAreaPageDocumentation: IDocumentationPageContent = {
-  description: <p>Displays single commands with controls for seamless clipboard copying.</p>,
+  description: <p>Displays single commands with a click action for seamless clipboard copying.</p>,
   codeExamples: [
     {
       title: 'Basic example',
       description: <>CopyArea copies the command provided to it to the clipboard.</>,
-      code: '<div className="p-1"><CopyArea command="npm i maincode-ui"/></div>',
+      code: `<div className="p-1">
+  <CopyArea command="npm i maincode-ui"/>
+</div>`,
       scope: { CopyArea },
     },
   ],
   props: [
-    { title: 'command', description: 'The command you want to be copied to the clipboard', required: true, type: 'string' },
-    { title: 'tooltip', description: 'The tooltip text you want to be shown on hover', required: false, type: 'string', defaultValue: 'Copy to clipboard' },
+    { title: 'command', description: 'The command copied to the clipboard', required: true, type: 'string' },
+    { title: 'tooltip', description: 'The tooltip text shown on hover', required: false, type: 'string', defaultValue: 'Copy to clipboard' },
     { title: 'className', description: 'Classes for custom styling', required: false, type: 'string', defaultValue: '""' },
   ],
   styles: [
