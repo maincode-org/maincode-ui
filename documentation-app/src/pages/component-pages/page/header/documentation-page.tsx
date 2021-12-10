500092e4ad309feec6127b3d672d2e359eea3ae5
Markido <markido@live.dk>
2021-12-10 01:29:31 +0100

    Page components documentation review done.

--- documentation-app/src/pages/component-pages/page/header/documentation-page.tsx
+++ documentation-app/src/pages/component-pages/page/header/documentation-page.tsx
@@ -1,13 +1,14 @@
-import { IDocumentationPageContent } from 'maincode-ui';
+import { IDocumentationPageContent, Header } from 'maincode-ui';
 import { basicCodeExample } from './examples';
 
 export const headerPageDocumentation: IDocumentationPageContent = {
-  description: <p>Customizable header bar.</p>,
+  description: <p>A customizable header bar. It is positioned as fixed by default, as used at the top of this page, but it can be used relatively like in the example below.</p>,
   codeExamples: [
     {
       title: 'Basic example',
       code: basicCodeExample,
-      enablePreview: false,
+      enablePreview: true,
+      scope: { Header },
     },
   ],
   props: [
