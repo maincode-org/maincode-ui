eb89b497c0b8c1883a87499625b7270e36489bbf
Markido <markido@live.dk>
2021-12-28 16:03:10 +0100

    Tailwind CSS working

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'publish',
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  },
+};
